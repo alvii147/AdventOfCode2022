5f3b025f9e9c4248df7af51329796f58f603f741
Zahin Zaman <alvii147@gmail.com>
2022-12-13 01:57:30 -0500

    add comments

--- Day_12_Hill_Climbing_Algorithm/Python/main.py
+++ Day_12_Hill_Climbing_Algorithm/Python/main.py
@@ -1,97 +1,139 @@
 import numpy as np
+from numpy.typing import NDArray
 import heapq
-from typing import List
+from typing import List, Tuple
 
 
+# class representing 2d elevation grid
 class Grid:
     def __init__(self, input_lines: List[str]):
+        # length of grid
         self.l = len(input_lines)
+        # width of grid
         self.w = len(input_lines[0])
+        # 2d elevation grid
         self.elevation = np.zeros((self.l, self.w), dtype=np.int8)
+        # starting point
         self.start = (0, 0)
+        # ending point
         self.end = (0, 0)
-        self.lowest = set()
+        # set of points with zero elevation
+        self.zero_elevation = set()
 
+        # iterate over grid rows
         for i in range(self.l):
+            # iterate over grid columns
             for j in range(self.w):
+                # get elevation value from character
                 v, is_start, is_end = self.get_elevation(input_lines[i][j])
 
+                # add to zero elevation set if zero
                 if v == 0:
-                    self.lowest.add((i, j))
+                    self.zero_elevation.add((i, j))
 
+                # set elevation value
                 self.elevation[i, j] = v
+
+                # set start or end points
                 if is_start:
                     self.start = (i, j)
                 elif is_end:
                     self.end = (i, j)
 
-    def get_elevation(self, char: str):
+    def get_elevation(self, char: str) -> Tuple[int, bool, bool]:
+        """
+        Get elevation value and whether it's a start or end point, given a character.
+        """
+        # whether it's a start point
         is_start = False
+        # whether it's an end point
         is_end = False
 
+        # a-z elevation character
         elevation_char = char
+
+        # start point
         if elevation_char == 'S':
             elevation_char = 'a'
             is_start = True
+        # end point
         elif elevation_char == 'E':
             elevation_char = 'z'
             is_end = True
 
+        # convert character to elevation value
         elevation = ord(elevation_char) - ord('a')
 
         return elevation, is_start, is_end
 
-    def get_neighbours(self, i, j, reverse=False):
+    def get_neighbours(self, i: int, j: int, visited: NDArray[np.bool_], reverse: bool = False) -> List[Tuple[int, int]]:
+        """
+        Get neighbouring indices. This returns only indices of neighbours that can be
+        travelled to from the current point. If reverse = True, this returns indices of
+        neighbours that can be travelled from to the current point.
+        """
         candidates = [
+            # top
             (i - 1, j),
+            # bottom
             (i + 1, j),
+            # left
             (i, j - 1),
+            # right
             (i, j + 1),
         ]
+        # set of neighbouring indices
         neighbours = set()
 
         for _i, _j in candidates:
+            # not a valid neighbour if indices out of range
             if _i < 0 or _i >= self.l or _j < 0 or _j >= self.w:
                 continue
 
-            if (_i, _j) in self.visited:
+            # not a valid neighbour if already visited
+            if visited[_i, _j]:
                 continue
 
+            # not a valid neighbour if higher difference in elevation than a single step
             if not reverse and self.elevation[_i, _j] - self.elevation[i, j] > 1:
                 continue
 
+            # not a valid neighbour if lower difference in elevation than a single step
             if reverse and self.elevation[i, j] - self.elevation[_i, _j] > 1:
                 continue
 
+            # add indices to set of neighbours
             neighbours.add((_i, _j))
 
         return neighbours
 
-    def single_source_shortest_path(self):
-        self.visited = {self.start}
-        queue = [self.start + (0,)]
+    def multiple_sources_shortest_paths(self, source: Tuple[int, int], reverse: bool) -> NDArray[np.int64]:
+        """
+        Perform Dikstra's algorithm to obtain shortest paths from given source to
+        multiple sources.
+        """
+        # boolean array representing whether indices have already been visited
+        visited = np.zeros((self.l, self.w), dtype=np.bool_)
+        visited[source] = True
+
+        # shortest distances from each pair of indices, initialized to inf
+        distances = np.full((self.l, self.w), np.iinfo(np.int64).max, dtype=np.int64)
+
+        # priority queue with distance and indices
+        # each item in queue is a 3-element tuple
+        # first element is distance, so priority queue is always ordered on distance
+        queue = [(0,) + source]
+
         while len(queue) > 0:
-            i, j, d = queue.pop(0)
-            if (i, j) == self.end:
-                break
-
-            for _i, _j in self.get_neighbours(i, j):
-                self.visited.add((_i, _j))
-                queue.append((_i, _j, d + 1))
-
-        return d
-
-    def multiple_sources_shortest_path(self):
-        self.visited = {self.end}
-        distances = {}
-        queue = [(0,) + self.end]
-        while len(queue) > 0:
+            # pop queue item
             d, i, j = heapq.heappop(queue)
-            if d < distances.get((i, j), float('inf')):
-                distances[(i, j)] = d
+            # relaxation
+            if d < distances[i, j]:
+                distances[i, j] = d
 
-            for _i, _j in self.get_neighbours(i, j, reverse=True):
-                self.visited.add((_i, _j))
+            # push neighbours onto queue
+            for _i, _j in self.get_neighbours(i, j, visited, reverse=reverse):
+                visited[_i, _j] = True
                 heapq.heappush(queue, (d + 1, _i, _j))
 
         return distances
@@ -104,8 +146,11 @@ if __name__ == '__main__':
     with open(file_path) as f:
         file_contents = f.read()
 
-    input_lines = file_contents.splitlines()
-    grid = Grid(input_lines)
-    print(grid.single_source_shortest_path())
-    distances = grid.multiple_sources_shortest_path()
-    print(min([distances.get((i, j), float('inf')) for (i, j) in grid.lowest]))
+    # create grid by parsing file contents
+    grid = Grid(file_contents.splitlines())
+    # compute shortest paths to end point
+    distances = grid.multiple_sources_shortest_paths(grid.end, True)
+    # shortest path from start to end
+    print(distances[grid.start])
+    # shortest path from any zero elevation point to end
+    print(min([distances[i, j] for (i, j) in grid.zero_elevation]))
